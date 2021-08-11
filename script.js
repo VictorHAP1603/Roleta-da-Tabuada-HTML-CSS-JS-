8d05ef7526ff88d445f5223faef0b90abca49148
MARCIO NOGUEIRA DE LIMA <marciolima76@hotmail.com>
2021-08-11 15:56:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let button = document.getElementById("generate1");
-button.onclick = function gerenate() {
-    let randomNum = Math.floor((Math.random() * 10) + 0);
-    let el = document.getElementById("ranNum1");
-    el.innerHTML = randomNum;
+let button1 = document.getElementById("generate1");
+button1.onclick = function gerenate() {
+    let randomNum1 = Math.floor((Math.random() * 10) + 0);
+    let el1 = document.getElementById("ranNum1");
+    el1.innerHTML = randomNum1;
 
-    let button = document.getElementById("generate2");
-    button.onclick = function gerenate() {
-        let randomNum = Math.floor((Math.random() * 10) + 0);
-        let el = document.getElementById("ranNum2");
-        el.innerHTML = randomNum;
+    let button2 = document.getElementById("generate2");
+    button2.onclick = function gerenate() {
+        let randomNum2 = Math.floor((Math.random() * 10) + 0);
+        let el2 = document.getElementById("ranNum2");
+        el2.innerHTML = randomNum2;
 
     }
 }
 
-function multiplica() {
+function verificar() {
+    let n1 = document.querySelector(".ranNum1").value;
+    let n2 = document.querySelector(".ranNum2").value;
+    let resultado = parseInt(n1) * parseInt(n2);
+    document.querySelector(".resultado").innerHTML = resultado;
 
 }
