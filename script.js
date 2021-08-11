220a27599815ce4da1a1849c03a7bb19be1d0770
VictorHugo <victor.hugo@newpost.com>
2021-08-11 16:04:47 -0300

    arruamando soma dos numeros

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 let button1 = document.getElementById("generate1");
 button1.onclick = function gerenate() {
-    let randomNum1 = Math.floor((Math.random() * 10) + 0);
-    let el1 = document.getElementById("ranNum1");
-    el1.innerHTML = randomNum1;
+  let randomNum1 = Math.floor(Math.random() * 10 + 0);
+  let el1 = document.getElementById("ranNum1");
+  el1.innerHTML = randomNum1;
+};
 
-    let button2 = document.getElementById("generate2");
-    button2.onclick = function gerenate() {
-        let randomNum2 = Math.floor((Math.random() * 10) + 0);
-        let el2 = document.getElementById("ranNum2");
-        el2.innerHTML = randomNum2;
-
-    }
-}
+let button2 = document.getElementById("generate2");
+button2.onclick = function gerenate() {
+  let randomNum2 = Math.floor(Math.random() * 10 + 0);
+  let el2 = document.getElementById("ranNum2");
+  el2.innerHTML = randomNum2;
+};
 
 function verificar() {
-    let n1 = document.querySelector(".ranNum1").value;
-    let n2 = document.querySelector(".ranNum2").value;
-    let resultado = parseInt(n1) * parseInt(n2);
-    document.querySelector(".resultado").innerHTML = resultado;
+  let n1 = Number(document.querySelector("#ranNum1").innerHTML);
+  let n2 = Number(document.querySelector("#ranNum2").innerHTML);
 
+  let resultado = n1 * n2;
+  document.querySelector(".resultado").innerHTML = resultado;
 }
